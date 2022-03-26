6bdf71b63dd6a94b259cd141044590b23618c61b
kenta Okazazki <mikadukim@gmail.com>
2022-03-26 16:45:07 +0900

    Add index.html and index.js

---
+++ index.ts
@@ -0,0 +1,19 @@
+let x = [1, 2, 3];
+
+const test_func = (leng) => {
+    if (x.length >= leng) {
+        console.log(x[1])
+    } else {
+        console.log('hogege')
+    }
+};
+
+const unko = {
+    color: 'pink',
+    size: 'large',
+    purfume: 'mint'
+};
+
+document.getElementsByTagName('button')[0].addEventListener('click', () => {
+    window.alert('Hogefw!!!')
+});
